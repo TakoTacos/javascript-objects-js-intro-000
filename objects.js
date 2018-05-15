6b2303223efce823d739ff8f7c69369fd0aa819d
Corey Lewis <clewis.development@gmail.com>
2018-05-15 04:59:06 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,5 +1,5 @@
 var playlist = {artistName: "songTitle"}
 
-function updatePlaylist ({playlist}, artist, song) {
-  return Object.assign({playlist}, artist, song)
+function updatePlaylist ({playlist}, artistName, songTitle) {
+  return Object.assign({playlist}, artistName, songTitle)
 }
